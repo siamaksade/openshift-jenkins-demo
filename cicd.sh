6a8ba252a9689f21af18c8a5d0098124f22a26a1
LutzLange <llange@redhat.com>
2017-06-30 07:21:28 +0000

    added cicd.sh

---
+++ cicd.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# 
+# CICD Demo
+# https://github.com/OpenShiftDemos/openshift-cd-demo
+#
+# you need to be loged in with oc for this to work
+#
+TEMPLATEPATH="$HOME/dev/co/openshift-cd-demo"
+
+DEVP="cicd-dev"
+STAGEP="cicd-stage"
+MGMTP="cicd-mgmt"
+
+echo "CLEANING UP FIRST - delete existing projects"
+# cleanup before start
+for PROJECT in $DEVP $STAGEP $MGMTP
+do
+  oc get project $PROJECT &>/dev/null && oc delete project $PROJECT
+done
+
+# check if projects are gone
+while oc get projects | grep -i terminating; do echo Waiting for Project Deletion; sleep 5; done
+
+echo "CREATING PROJECTS"
+# Create the projects
+oc new-project $DEVP --display-name="CI/CD Tasks - Dev"
+oc new-project $STAGEP --display-name="CI/CD Tasks - Stage"
+oc new-project $MGMTP --display-name="CI/CD Management"
+
+# Allow the jenkins to access our projects
+oc policy add-role-to-user edit system:serviceaccount:$MGMTP:jenkins -n $DEVP
+oc policy add-role-to-user edit system:serviceaccount:$MGMTP:jenkins -n $STAGEP
+
+echo "STARTING ROLL OUT"
+# Lets roll
+oc process -f $TEMPLATEPATH/cicd-template.yaml -p DEV_PROJECT=$DEVP -p STAGE_PROJECT=$STAGEP | oc create -f - -n $MGMTP
