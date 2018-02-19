a7f8e68aa1f1a2446e33a8225b663dc7995c79ac
root <root@osemaster.c.jens-walkthrough.internal>
2018-02-19 10:47:07 +0000

    provisionig / reprovisioning script added

---
+++ cicd.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# 
+# CICD Demo
+# https://github.com/OpenShiftDemos/openshift-cd-demo
+#
+# you need to be loged in with oc for this to work
+#
+TEMPLATEPATH="."
+
+USER=$(oc whoami)
+test -z "$USER" && { echo "please login to oc with your USERNAME - aborting $0" ; exit ; }
+
+DEVP="cicd-dev-$USER"
+STAGEP="cicd-stage-$USER"
+MGMTP="cicd-mgmt-$USER"
+
+echo "CLEANING UP FIRST - delete existing projects"
+# cleanup before start
+for PROJECT in $DEVP $STAGEP $MGMTP
+do
+  oc get project $PROJECT &>/dev/null && oc delete project $PROJECT
+done
+
+# check if projects are gone
+while oc get projects | grep -i terminating; do echo Waiting for Project Deletion; sleep 5; done
+
+echo "CREATING PROJECTS"
+# Create the projects
+oc new-project $DEVP --display-name="CI/CD Tasks - Dev"
+oc new-project $STAGEP --display-name="CI/CD Tasks - Stage"
+oc new-project $MGMTP --display-name="CI/CD Management"
+
+# Allow the jenkins to access our projects
+oc policy add-role-to-user edit system:serviceaccount:$MGMTP:jenkins -n $DEVP
+oc policy add-role-to-user edit system:serviceaccount:$MGMTP:jenkins -n $STAGEP
+
+echo "STARTING ROLL OUT"
+# Lets roll
+oc process -f $TEMPLATEPATH/cicd-template.yaml -p DEV_PROJECT=$DEVP -p STAGE_PROJECT=$STAGEP | oc create -f - -n $MGMTP
